537f77a8c4c05dd188b744b3419c4b1d7eaab3d3
maspypy <maspypy@gmail.com>
2020-03-10 11:40:53 +0900

    fix judge for yukicoder

--- ReiwaSequence/tests/judge.py
+++ ReiwaSequence/tests/judge.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3.8
-import argparse
 import numpy as np
+import sys
+
+
+def rime_judge_streams():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--infile')
+    parser.add_argument('--outfile')
+    parser.add_argument('--difffile')
+    args = parser.parse_args()
+    inf = open(args.infile)
+    difff = open(args.difffile)
+    outf = open(args.outfile)
+    return inf, difff, outf
+
+
+def yuki_judge_streams():
+    args = sys.argv
+    inf = open(args[1])
+    difff = open(args[2])
+    outf = sys.stdin
+    return inf, difff, outf
+
+
+inf, difff, outf = rime_judge_streams()
+# inf, difff, outf = yuki_judge_streams()
 
 
 class WrongAnswer(Exception):
     pass
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--infile')
-parser.add_argument('--outfile')
-parser.add_argument('--difffile')
-args = parser.parse_args()
-
-
 def is_AC():
-    inf = open(args.infile)
-    difff = open(args.difffile)
-    outf = open(args.difffile)
-
     _ = int(inf.readline())
     A = np.array(inf.readline().split(), np.int32)
 
