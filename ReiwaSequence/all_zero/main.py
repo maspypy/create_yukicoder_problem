f6018d9cb7adeafbfc4ac96d76e4a7de442ccf64
maspypy <maspypy@gmail.com>
2020-03-15 21:48:41 +0900

    add WA solution

---
+++ ReiwaSequence/all_zero/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.8
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+import numpy as np
+
+
+def solve(A):
+    return np.zeros_like(A)
+
+
+if __name__ == '__main__':
+    N = int(readline())
+    A = np.array(read().split(), np.int32)
+    B = solve(A)
+    if B is None:
+        print('No')
+    else:
+        print('Yes')
+        print(' '.join(B.astype(str)))
